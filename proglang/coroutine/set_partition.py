f424395472388be69704a1bf7d5deb1b6a19f78b
Live session user <luk036@gmail.com>
2019-03-26 20:51:59 +0800

    format change

--- proglang/coroutine/set_partition.py
+++ proglang/coroutine/set_partition.py
@@ -1,5 +1,5 @@
 # Set Partition
-# 
+#
 # A set partition of the set [n] = {1,2,3,...,n} is a collection B0,
 # B1, ... Bj of disjoint subsets of [n] whose union is [n]. Each Bj
 # is called a block. Below we show the partitions of [4]. The periods
@@ -20,8 +20,8 @@
 #
 #   1 block:  0000
 #   2 blocks: 0001, 0010, 0100, 0111, 0011, 0101, 0110
-#   3 blocks: 0122, 0121, 0112, 0120, 0102, 
-# 
+#   3 blocks: 0122, 0121, 0112, 0120, 0102,
+#
 # ...more
 #
 # Reference:
@@ -30,25 +30,28 @@
 # 201-208. Also downloadable from
 # http://webhome.cs.uvic.ca/~ruskey/Publications/SimpleGray/SimpleGray.html
 
+
 def stirling2nd(n, k):
     '''
     Stirling number of second kind. Note that this function is for
     testing purpose only and is slow because of reducdant calculation.
     '''
-    if (k >= n or k <= 1): return 1
+    if (k >= n or k <= 1):
+        return 1
     return stirling2nd(n-1, k-1) + k * stirling2nd(n-1, k)
 
+
 def set_partition(n, k):
-    if k%2 == 0: 
-        for x, y in GEN0_even( n, k ):
+    if k % 2 == 0:
+        for x, y in GEN0_even(n, k):
             yield x, y
-    else: 
-        for x, y in GEN0_odd( n, k):
+    else:
+        for x, y in GEN0_odd(n, k):
             yield x, y
     assert stirling2nd(n, k) == cnt
 
 
-def Move ( x, y ):
+def Move(x, y):
     yield x, y
 
 
@@ -59,18 +62,19 @@ def Move ( x, y ):
 # 4. last(S(n,k,1)) = 012...(k-1)0^{n-k}
 # Note that first(S'(n,k,p)) = last(S(n,k,p))
 
-def GEN0_even ( n, k ):
+def GEN0_even(n, k):
     ''' S(n,k,0) even k '''
-    if not (k > 0 and k < n): return
+    if not (k > 0 and k < n):
+        return
     for x, y in GEN0_odd(n-1, k-1):    # S(n-1, k-1, 0).(k-1)
         yield x, y
-    for x, y in Move(n-1,k-1):
+    for x, y in Move(n-1, k-1):
         yield x, y
     for x, y in GEN1_even(n-1, k):     # S(n-1, k, 1).(k-1)
         yield x, y
     for x, y in Move(n, k-2):
         yield x, y
-    for x, y in NEG1_even(n-1, k):     # S'(n-1, k, 1).(k-2)  
+    for x, y in NEG1_even(n-1, k):     # S'(n-1, k, 1).(k-2)
         yield x, y
     for i in range(k-3, 0, -2):
         for x, y in Move(n, i):
@@ -83,13 +87,14 @@ def GEN0_even ( n, k ):
             yield x, y
 
 
-def NEG0_even ( n, k ):
+def NEG0_even(n, k):
     ''' S'(n,k,0) even k '''
-    if not (k > 0 and k < n): return
+    if not (k > 0 and k < n):
+        return
     for i in range(1, k-2, 2):
         for x, y in GEN1_even(n-1, k):   # S(n-1, k, 1).(i-1)
             yield x, y
-        for x, y in Move(n, i):      
+        for x, y in Move(n, i):
             yield x, y
         for x, y in NEG1_even(n-1, k):   # S'(n-1, k, 1).i
             yield x, y
@@ -101,15 +106,16 @@ def NEG0_even ( n, k ):
         yield x, y
     for x, y in NEG1_even(n-1, k):     # S(n-1, k, 1).(k-1)
         yield x, y
-    for x, y in Move(n-1, 0):      
+    for x, y in Move(n-1, 0):
         yield x, y
     for x, y in NEG0_odd(n-1, k-1):    # S(n-1, k-1, 1).(k-1)
         yield x, y
 
 
-def GEN1_even ( n, k ):
+def GEN1_even(n, k):
     ''' S(n,k,1) even k '''
-    if not (k > 0 and k < n): return
+    if not (k > 0 and k < n):
+        return
     for x, y in GEN1_odd(n-1, k-1):
         yield x, y
     for x, y in Move(k, k-1):
@@ -131,38 +137,40 @@ def GEN1_even ( n, k ):
             yield x, y
 
 
-def NEG1_even ( n, k ):
+def NEG1_even(n, k):
     ''' S'(n,k,1) even k '''
-    if not (k > 0 and k < n): return
+    if not (k > 0 and k < n):
+        return
     for i in range(1, k-2, 2):
-        for x, y in NEG1_even(n-1, k): 
+        for x, y in NEG1_even(n-1, k):
             yield x, y
-        for x, y in Move(n, i):      
+        for x, y in Move(n, i):
             yield x, y
-        for x, y in GEN1_even(n-1, k): 
+        for x, y in GEN1_even(n-1, k):
             yield x, y
         for x, y in Move(n, i+1):
             yield x, y
-    for x, y in NEG1_even(n-1, k):     
+    for x, y in NEG1_even(n-1, k):
         yield x, y
     for x, y in Move(n, k-1):
         yield x, y
-    for x, y in GEN1_even(n-1, k):    
+    for x, y in GEN1_even(n-1, k):
         yield x, y
-    for x, y in Move(k, 0):      
+    for x, y in Move(k, 0):
         yield x, y
-    for x, y in NEG1_odd(n-1, k-1):  
+    for x, y in NEG1_odd(n-1, k-1):
         yield x, y
 
 
-def GEN0_odd ( n, k ):
+def GEN0_odd(n, k):
     ''' S(n,k,0) odd k '''
-    if not (k > 1 and k < n): return
-    for x, y in GEN1_even(n-1, k-1): 
+    if not (k > 1 and k < n):
+        return
+    for x, y in GEN1_even(n-1, k-1):
         yield x, y
-    for x, y in Move(k,k-1):
+    for x, y in Move(k, k-1):
         yield x, y
-    for x, y in NEG1_odd(n-1, k): 
+    for x, y in NEG1_odd(n-1, k):
         yield x, y
     for i in range(k-2, 0, -2):
         for x, y in Move(n, i):
@@ -171,23 +179,24 @@ def GEN0_odd ( n, k ):
             yield x, y
         for x, y in Move(n, i-1):
             yield x, y
-        for x, y in NEG1_odd(n-1, k): 
+        for x, y in NEG1_odd(n-1, k):
             yield x, y
 
 
-def NEG0_odd ( n, k ):
+def NEG0_odd(n, k):
     ''' S'(n,k,0) odd k '''
-    if not (k > 1 and k < n): return
+    if not (k > 1 and k < n):
+        return
     for i in range(1, k-1, 2):
-        for x, y in GEN1_odd(n-1, k): 
+        for x, y in GEN1_odd(n-1, k):
             yield x, y
-        for x, y in Move(n, i):      
+        for x, y in Move(n, i):
             yield x, y
-        for x, y in NEG1_odd(n-1, k):  
+        for x, y in NEG1_odd(n-1, k):
             yield x, y
         for x, y in Move(n, i+1):
             yield x, y
-    for x, y in GEN1_odd(n-1, k): 
+    for x, y in GEN1_odd(n-1, k):
         yield x, y
     for x, y in Move(k, 0):
         yield x, y
@@ -195,14 +204,15 @@ def NEG0_odd ( n, k ):
         yield x, y
 
 
-def GEN1_odd ( n, k ):
+def GEN1_odd(n, k):
     ''' S(n,k,1) odd k '''
-    if not (k > 1 and k < n): return
+    if not (k > 1 and k < n):
+        return
     for x, y in GEN0_even(n-1, k-1):
         yield x, y
-    for x, y in Move(n-1,k-1):
+    for x, y in Move(n-1, k-1):
         yield x, y
-    for x, y in GEN1_odd(n-1, k): 
+    for x, y in GEN1_odd(n-1, k):
         yield x, y
     for i in range(k-2, 0, -2):
         for x, y in Move(n, i):
@@ -211,23 +221,24 @@ def GEN1_odd ( n, k ):
             yield x, y
         for x, y in Move(n, i-1):
             yield x, y
-        for x, y in GEN1_odd(n-1, k): 
+        for x, y in GEN1_odd(n-1, k):
             yield x, y
 
 
-def NEG1_odd ( n, k ):
+def NEG1_odd(n, k):
     ''' S'(n,k,1) odd k '''
-    if not (k > 1 and k < n): return
+    if not (k > 1 and k < n):
+        return
     for i in range(1, k-1, 2):
-        for x, y in NEG1_odd(n-1, k): 
+        for x, y in NEG1_odd(n-1, k):
             yield x, y
-        for x, y in Move(n, i):      
+        for x, y in Move(n, i):
             yield x, y
-        for x, y in GEN1_odd(n-1, k):  
+        for x, y in GEN1_odd(n-1, k):
             yield x, y
         for x, y in Move(n, i+1):
             yield x, y
-    for x, y in NEG1_odd(n-1, k): 
+    for x, y in NEG1_odd(n-1, k):
         yield x, y
     for x, y in Move(n-1, 0):
         yield x, y
@@ -239,9 +250,9 @@ if __name__ == "__main__":
     n, k = 4, 2
     b = [0 for i in range(n-k+1)] + list(range(k))
     cnt = 1
-    for x, y in set_partition(n,k):
+    for x, y in set_partition(n, k):
         old = b[x]
-        b[x] = y    
+        b[x] = y
         cnt += 1
         print(b[1:], ": Move {} from block {} to {}".format(x, old, y))
     assert stirling2nd(n, k) == cnt
--- proglang/coroutine/set_partition3.py
+++ proglang/coroutine/set_partition3.py
@@ -31,22 +31,25 @@
 # 201-208. Also downloadable from
 # http://webhome.cs.uvic.ca/~ruskey/Publications/SimpleGray/SimpleGray.html
 
+
 def stirling2nd(n, k):
     '''
     Stirling number of second kind. Note that this function is for
     testing purpose only and is slow because of reducdant calculation.
     '''
-    if (k >= n or k <= 1): return 1
+    if (k >= n or k <= 1):
+        return 1
     return stirling2nd(n-1, k-1) + k * stirling2nd(n-1, k)
 
+
 def set_partition(n, k):
-    if k%2 == 0:
-        yield from GEN0_even( n, k )
+    if k % 2 == 0:
+        yield from GEN0_even(n, k)
     else:
-        yield from GEN0_odd( n, k)
+        yield from GEN0_odd(n, k)
 
 
-def Move ( x, y ):
+def Move(x, y):
     yield x, y
 
 
@@ -57,127 +60,126 @@ def Move ( x, y ):
 # 4. last(S(n,k,1)) = 012...(k-1)0^{n-k}
 # Note that first(S'(n,k,p)) = last(S(n,k,p))
 
-def GEN0_even ( n, k ):
+def GEN0_even(n, k):
     ''' S(n,k,0) even k '''
-    if not (k > 0 and k < n): return
-    yield from GEN0_odd(n-1, k-1)
-    yield from Move(n-1,k-1)
-    yield from GEN1_even(n-1, k)
-    yield from Move(n, k-2)
-    yield from NEG1_even(n-1, k)
-    for i in range(k-3, 0, -2):
-        yield from Move(n, i)
+    if k > 0 and k < n:
+        yield from GEN0_odd(n-1, k-1)
+        yield from Move(n-1, k-1)
         yield from GEN1_even(n-1, k)
-        yield from Move(n, i-1)
+        yield from Move(n, k-2)
         yield from NEG1_even(n-1, k)
+        for i in range(k-3, 0, -2):
+            yield from Move(n, i)
+            yield from GEN1_even(n-1, k)
+            yield from Move(n, i-1)
+            yield from NEG1_even(n-1, k)
 
 
-def NEG0_even ( n, k ):
+def NEG0_even(n, k):
     ''' S'(n,k,0) even k '''
-    if not (k > 0 and k < n): return
-    for i in range(1, k-2, 2):
+    if k > 0 and k < n:
+        for i in range(1, k-2, 2):
+            yield from GEN1_even(n-1, k)
+            yield from Move(n, i)
+            yield from NEG1_even(n-1, k)
+            yield from Move(n, i+1)
         yield from GEN1_even(n-1, k)
-        yield from Move(n, i)
+        yield from Move(n, k-1)
         yield from NEG1_even(n-1, k)
-        yield from Move(n, i+1)
-    yield from GEN1_even(n-1, k)
-    yield from Move(n, k-1)
-    yield from NEG1_even(n-1, k)
-    yield from Move(n-1, 0)
-    yield from NEG0_odd(n-1, k-1)
+        yield from Move(n-1, 0)
+        yield from NEG0_odd(n-1, k-1)
 
 
-def GEN1_even ( n, k ):
+def GEN1_even(n, k):
     ''' S(n,k,1) even k '''
-    if not (k > 0 and k < n): return
-    yield from GEN1_odd(n-1, k-1)
-    yield from Move(k, k-1)
-    yield from NEG1_even(n-1, k)
-    yield from Move(n, k-2)
-    yield from GEN1_even(n-1, k)
-    for i in range(k-3, 0, -2):
-        yield from Move(n, i)
+    if k > 0 and k < n:
+        yield from GEN1_odd(n-1, k-1)
+        yield from Move(k, k-1)
         yield from NEG1_even(n-1, k)
-        yield from Move(n, i-1)
+        yield from Move(n, k-2)
         yield from GEN1_even(n-1, k)
+        for i in range(k-3, 0, -2):
+            yield from Move(n, i)
+            yield from NEG1_even(n-1, k)
+            yield from Move(n, i-1)
+            yield from GEN1_even(n-1, k)
 
 
-def NEG1_even ( n, k ):
+def NEG1_even(n, k):
     ''' S'(n,k,1) even k '''
-    if not (k > 0 and k < n): return
-    for i in range(1, k-2, 2):
+    if k > 0 and k < n:
+        for i in range(1, k-2, 2):
+            yield from NEG1_even(n-1, k)
+            yield from Move(n, i)
+            yield from GEN1_even(n-1, k)
+            yield from Move(n, i+1)
         yield from NEG1_even(n-1, k)
-        yield from Move(n, i)
+        yield from Move(n, k-1)
         yield from GEN1_even(n-1, k)
-        yield from Move(n, i+1)
-    yield from NEG1_even(n-1, k)
-    yield from Move(n, k-1)
-    yield from GEN1_even(n-1, k)
-    yield from Move(k, 0)
-    yield from NEG1_odd(n-1, k-1)
+        yield from Move(k, 0)
+        yield from NEG1_odd(n-1, k-1)
 
 
-def GEN0_odd ( n, k ):
+def GEN0_odd(n, k):
     ''' S(n,k,0) odd k '''
-    if not (k > 1 and k < n): return
-    yield from GEN1_even(n-1, k-1)
-    yield from Move(k,k-1)
-    yield from NEG1_odd(n-1, k)
-    for i in range(k-2, 0, -2):
-        yield from Move(n, i)
-        yield from GEN1_odd(n-1, k)
-        yield from Move(n, i-1)
+    if k > 1 and k < n:
+        yield from GEN1_even(n-1, k-1)
+        yield from Move(k, k-1)
         yield from NEG1_odd(n-1, k)
+        for i in range(k-2, 0, -2):
+            yield from Move(n, i)
+            yield from GEN1_odd(n-1, k)
+            yield from Move(n, i-1)
+            yield from NEG1_odd(n-1, k)
 
 
-def NEG0_odd ( n, k ):
+def NEG0_odd(n, k):
     ''' S'(n,k,0) odd k '''
-    if not (k > 1 and k < n): return
-    for i in range(1, k-1, 2):
+    if k > 1 and k < n:
+        for i in range(1, k-1, 2):
+            yield from GEN1_odd(n-1, k)
+            yield from Move(n, i)
+            yield from NEG1_odd(n-1, k)
+            yield from Move(n, i+1)
         yield from GEN1_odd(n-1, k)
-        yield from Move(n, i)
-        yield from NEG1_odd(n-1, k)
-        yield from Move(n, i+1)
-    yield from GEN1_odd(n-1, k)
-    yield from Move(k, 0)
-    yield from NEG1_even(n-1, k-1)
+        yield from Move(k, 0)
+        yield from NEG1_even(n-1, k-1)
 
 
-def GEN1_odd ( n, k ):
+def GEN1_odd(n, k):
     ''' S(n,k,1) odd k '''
-    if not (k > 1 and k < n): return
-    yield from GEN0_even(n-1, k-1)
-    yield from Move(n-1,k-1)
-    yield from GEN1_odd(n-1, k)
-    for i in range(k-2, 0, -2):
-        yield from Move(n, i)
-        yield from NEG1_odd(n-1, k)
-        yield from Move(n, i-1)
+    if k > 1 and k < n:
+        yield from GEN0_even(n-1, k-1)
+        yield from Move(n-1, k-1)
         yield from GEN1_odd(n-1, k)
+        for i in range(k-2, 0, -2):
+            yield from Move(n, i)
+            yield from NEG1_odd(n-1, k)
+            yield from Move(n, i-1)
+            yield from GEN1_odd(n-1, k)
 
 
-def NEG1_odd ( n, k ):
+def NEG1_odd(n, k):
     ''' S'(n,k,1) odd k '''
-    if not (k > 1 and k < n): return
-    for i in range(1, k-1, 2):
+    if k > 1 and k < n:
+        for i in range(1, k-1, 2):
+            yield from NEG1_odd(n-1, k)
+            yield from Move(n, i)
+            yield from GEN1_odd(n-1, k)
+            yield from Move(n, i+1)
         yield from NEG1_odd(n-1, k)
-        yield from Move(n, i)
-        yield from GEN1_odd(n-1, k)
-        yield from Move(n, i+1)
-    yield from NEG1_odd(n-1, k)
-    yield from Move(n-1, 0)
-    yield from NEG0_even(n-1, k-1)
+        yield from Move(n-1, 0)
+        yield from NEG0_even(n-1, k-1)
 
 
 if __name__ == "__main__":
     n, k = 5, 3
     b = [0 for i in range(n-k+1)] + list(range(k))
     cnt = 1
-    for x, y in set_partition(n,k):
+    for x, y in set_partition(n, k):
         old = b[x]
         b[x] = y
         cnt += 1
         print(b[1:], ": Move {} from block {} to {}".format(x, old, y))
     assert stirling2nd(n, k) == cnt
     print("Done.")
-
